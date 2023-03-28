cb7746140e7c72d9ba138cd50e642398b9bca5f5
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2023-03-28 08:28:42 +0530

    react class1 done

--- src/session-1/UseState.jsx
+++ src/session-1/UseState.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+
+
 export const UseState = () => {
   const [count, setCount] = useState(0);
   // console.log(useState(0)) 
@@ -8,9 +10,31 @@ export const UseState = () => {
       <h1 style={{ color: count > 10 ? "red" : "black" }}>{count}</h1>
       <button onClick={() => setCount(count => count + 1)}>+</button>
       <button onClick={() => setCount(count => count - 1)}>-</button>
-      <button onClick={() => setCount(count => 0)}>Reset</button>
+      <button onClick={() => setCount(count => 0)} style={{ marginBottom: "10px" }}>Reset</button>
+      <Fruits />
     </>
 
+  )
+}
+
+
+const fruits = [
+  { id: 1, name: "Apple", price: 200 },
+  { id: 2, name: "Orange", price: 150 },
+  { id: 3, name: "Grapes", price: 90 },
+  { id: 4, name: "Pineapple", price: 70 }
+];
+
+const Fruits = () => {
+  const [shouldHighlight, setShouldHighlight] = useState(false);
+  return (
+    <>
+      <h1>Fruits</h1>
+      {fruits.map(({ name, price }) => <p style={{ color: shouldHighlight && price > 100 ? "red" : "black" }}>Name: {name}, Price: {price}</p>)}
+      <button onClick={() => setShouldHighlight(true)}>Highlight Costly Fruits</button>
+      <button onClick={() => setShouldHighlight(false)}>Reset</button>
+      <button onClick={() => setShouldHighlight(shouldHighlight => shouldHighlight ? false : true)}>Toggle</button> {/* !shouldHighlight */}
+    </>
   )
 }
 
